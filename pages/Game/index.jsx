2e66c42d26b87b4d4260b7b4eff26aaa6217f644
Renatogranados <renatogranadosgt@gmail.com>
2022-04-05 10:38:57 -0600

    feat: add design for one player

--- pages/Game/index.jsx
+++ pages/Game/index.jsx
@@ -1,5 +1,57 @@
+import Layout from 'components/Layout';
 import React from 'react';
 
+import styles from './styles.module.css';
+
 export default function Game() {
-  return <div>Game</div>;
+  return (
+    <Layout titleHeader='Unjugador' href='/OnePlayer'>
+      <div>
+        <h2 className={styles.title}>Adivine la palabra</h2>
+      </div>
+
+      <section className={styles.mainGameContainer}>
+        <div className={styles.mainGame}>
+          <div className={styles.base}></div>
+          <div className={styles.column}></div>
+          <div className={styles.arm}></div>
+          <div className={styles.rope}></div>
+
+          <div className={styles.head}></div>
+          <div className={styles.leftArm}></div>
+          <div className={styles.rightArm}></div>
+          <div className={styles.body}></div>
+          <div className={styles.leftLeg}></div>
+          <div className={styles.rightLeg}></div>
+        </div>
+      </section>
+
+      <section className={styles.wordContainer}>
+        <div className={styles.line}>
+          <p className={styles.wordLetter}>C</p>
+        </div>
+        <div className={styles.line}>
+          <p className={styles.wordLetter}>C</p>
+        </div>
+        <div className={styles.line}>
+          <p className={styles.wordLetter}>M</p>
+        </div>
+        <div className={styles.line}>
+          <p className={styles.wordLetter}>C</p>
+        </div>
+        <div className={styles.line}>
+          <p className={styles.wordLetter}>C</p>
+        </div>
+        <div className={styles.line}>
+          <p className={styles.wordLetter}>C</p>
+        </div>
+        <div className={styles.line}>
+          <p className={styles.wordLetter}>C</p>
+        </div>
+        <div className={styles.line}>
+          <p className={styles.wordLetter}>C</p>
+        </div>
+      </section>
+    </Layout>
+  );
 }
--- pages/OnePlayer/index.jsx
+++ pages/OnePlayer/index.jsx
@@ -1,10 +1,20 @@
 import Layout from 'components/Layout';
+import LayoutButton from 'components/LayoutButton';
+import LinkToPage from 'components/LinkToPage';
 import React from 'react';
+
+import styles from './styles.module.css';
 
 export default function OnePlayer() {
   return (
-    <Layout titleHeader='Un jugador'>
-      <h2>Un jugador</h2>
+    <Layout titleHeader='Un jugador' href='/'>
+      <h2 className={styles.title}>Seleccione una dificultad</h2>
+
+      <LayoutButton>
+        <LinkToPage contain='Fácil' href='/Game'></LinkToPage>
+        <LinkToPage contain='Medio' href='/Game'></LinkToPage>
+        <LinkToPage contain='Difícil' href='/Game'></LinkToPage>
+      </LayoutButton>
     </Layout>
   );
 }
