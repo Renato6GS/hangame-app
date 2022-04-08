c802b6b0fcc4eed9827268343306dc5da0c7e06d
Renatogranados <renatogranadosgt@gmail.com>
2022-04-07 21:22:36 -0600

    feat: add seo and responsive design for desktop

--- pages/OnePlayer/index.jsx
+++ pages/OnePlayer/index.jsx
@@ -2,19 +2,27 @@ import Layout from 'components/Layout';
 import LayoutButton from 'components/LayoutButton';
 import LinkToPage from 'components/LinkToPage';
 import React from 'react';
+import Head from 'next/head';
 
 import styles from './styles.module.css';
 
 export default function OnePlayer() {
   return (
-    <Layout titleHeader='Un jugador' href='/'>
-      <h2 className={styles.title}>Seleccione una dificultad</h2>
+    <>
+      <Head>
+        <title>Hangame - Un jugador</title>
+        <meta name='description' content='Hangman game with one player' />
+        <link rel='icon' href='/logo.ico' />
+      </Head>
+      <Layout titleHeader='Un jugador' href='/'>
+        <h2 className={styles.title}>Seleccione una dificultad</h2>
 
-      <LayoutButton>
-        <LinkToPage contain='Fácil' href='/Game/easy'></LinkToPage>
-        <LinkToPage contain='Medio' href='/Game/medium'></LinkToPage>
-        <LinkToPage contain='Difícil' href='/Game/hard'></LinkToPage>
-      </LayoutButton>
-    </Layout>
+        <LayoutButton>
+          <LinkToPage contain='Fácil' href='/Game/easy'></LinkToPage>
+          <LinkToPage contain='Medio' href='/Game/medium'></LinkToPage>
+          <LinkToPage contain='Difícil' href='/Game/hard'></LinkToPage>
+        </LayoutButton>
+      </Layout>
+    </>
   );
 }
