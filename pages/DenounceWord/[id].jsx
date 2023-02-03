1894b5825a9571125445f47f563bcf71ed27856b
Renatogranados <renatogranadosgt@gmail.com>
2023-02-02 22:27:54 -0600

    add translations to denounce page modals

--- pages/DenounceWord/[id].jsx
+++ pages/DenounceWord/[id].jsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/router";
 
 import styles from "./styles.module.css";
 import { useI18N } from "context/i18n";
+import HeadSEO from "components/HeadSEO";
 
 export default function DenounceWord({ word }) {
   const router = useRouter();
@@ -23,51 +24,54 @@ export default function DenounceWord({ word }) {
 
     if (res.status || res) {
       showModal({
-        title: "Denuncia realizada",
-        message: "Gracias por tu denuncia, la revisaremos y tomaremos las medidas necesarias.",
+        title: t("DENOUNCE_SENT"),
+        message: t("DENOUNCE_SENT_DESCRIPTION"),
       }).then(() => router.push("/"));
     } else {
       showModal({
         type: "error",
-        title: "Error",
-        message: "Ha ocurrido un error, por favor intenta de nuevo más tarde.",
+        title: t("ERROR"),
+        message: t("DENOUNCE_ERROR_DESCRIPTION"),
       });
     }
   };
 
   return (
-    <Layout>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <Link href="/">
-          <a className={styles.return}>{t("RETURN_MAIN_MENU")}</a>
-        </Link>
-        <h1>
-          {t("DENOUNCE_WORD")}: &quot;{word}&quot;
-        </h1>
-        <p>{t("DENOUNCE_DESCRIPTION_1")}</p>
-        <p>{t("DENOUNCE_DESCRIPTION_2", word)}</p>
-        <h3>{t("CATEGORIES")}</h3>
-        <div className={styles.categoriesContainer}>
-          <div>
-            <input type="radio" name="category" id="typo" required />
-            <label htmlFor="typo">{t("DENOUNCE_TYPO")}</label>
+    <>
+      <HeadSEO title="DENOUNCE_WORD" description={"DENOUNCE_WORD_SEO"} />
+      <Layout>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <Link href="/">
+            <a className={styles.return}>{t("RETURN_MAIN_MENU")}</a>
+          </Link>
+          <h1>
+            {t("DENOUNCE_WORD")}: &quot;{word}&quot;
+          </h1>
+          <p>{t("DENOUNCE_DESCRIPTION_1")}</p>
+          <p>{t("DENOUNCE_DESCRIPTION_2", word)}</p>
+          <h3>{t("CATEGORIES")}</h3>
+          <div className={styles.categoriesContainer}>
+            <div>
+              <input type="radio" name="category" id="typo" required />
+              <label htmlFor="typo">{t("DENOUNCE_TYPO")}</label>
+            </div>
+            <div>
+              <input type="radio" name="category" id="offensive" required />
+              <label htmlFor="offensive">{t("DENOUNCE_INAPROPRIATE")}</label>
+            </div>
           </div>
-          <div>
-            <input type="radio" name="category" id="offensive" required />
-            <label htmlFor="offensive">{t("DENOUNCE_INAPROPRIATE")}</label>
-          </div>
-        </div>
-        <h3>{t("COMMENTS")}</h3>
-        <textarea
-          placeholder={t("COMMENTS_PLACEHOLDER")}
-          cols="30"
-          rows="6"
-          name="commentary"
-          maxLength={200}
-          required></textarea>
-        <button type="submit">{t("SEND")}</button>
-      </form>
-    </Layout>
+          <h3>{t("COMMENTS")}</h3>
+          <textarea
+            placeholder={t("COMMENTS_PLACEHOLDER")}
+            cols="30"
+            rows="6"
+            name="commentary"
+            maxLength={200}
+            required></textarea>
+          <button type="submit">{t("SEND")}</button>
+        </form>
+      </Layout>
+    </>
   );
 }
 
