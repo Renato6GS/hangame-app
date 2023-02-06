86963421963853c454c7b8b1ff2d82f804243e9c
Renatogranados <renatogranadosgt@gmail.com>
2023-02-06 09:56:11 -0600

    feat: add tags to footer

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import styles from "./styles.module.css";
 import { useI18N } from "context/i18n";
+import { CohereIcon, MongoDbIcon, NextIcon } from "components/icons";
 
 export default function Footer() {
   const { t } = useI18N();
@@ -9,10 +10,28 @@ export default function Footer() {
     <footer className={styles.footer}>
       <h4 className={styles.description}>
         {t("AUTHOR")}
-        <a className={styles.link} href="https://www.renatogranados.dev" target="_blank" rel="noopener noreferrer">
+        <a
+          className={`${styles.tag} ${styles.link}`}
+          href="https://www.renatogranados.dev"
+          target="_blank"
+          rel="noopener noreferrer">
           @Renato6GS
         </a>
       </h4>
+      <section className={styles.tagsContainer}>
+        <a className={styles.tag} href="https://www.mongodb.com/" target={"_blank"} rel="noreferrer">
+          <NextIcon />
+          Next.js
+        </a>
+        <a className={styles.tag} href="https://cohere.ai/" target={"_blank"} rel="noreferrer">
+          <MongoDbIcon />
+          MongoDB
+        </a>
+        <a className={styles.tag}>
+          <CohereIcon />
+          Co:here
+        </a>
+      </section>
     </footer>
   );
 }
